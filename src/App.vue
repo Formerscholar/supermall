70eeaf037361c2ba13234136c05e4dd08ee1d9fc
Formerscholar <2319315514@qq.com>
2020-02-03 16:04:35 +0800

    2/3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+  </div>
 </template>
 <script>
+import MainTabBar from "components/content/MainTabBar/MainTabBar";
 export default {
   name: "app",
-  components: {}
+  components: {
+    MainTabBar
+  }
 };
 </script>
 
