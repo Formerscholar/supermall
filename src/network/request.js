4384ad72af0c2f9838a00a0a5d4e047bdce04752
Formerscholar <2319315514@qq.com>
2020-02-09 17:57:00 +0800

    2/9

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,8 @@
+//主接口 http://123.207.32.32:8000/api/hy 备用接口 http://106.54.54.237:8000/api/hy
 import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://106.54.54.237:8000/api/hy', //主接口 http://123.207.32.32:8000/api/hy 备用接口 http://106.54.54.237:8000/api/hy
+    baseURL: 'http://123.207.32.32:8000/api/hy',
     timeout: 5000
   })
   instance.interceptors.request.use(config => {
